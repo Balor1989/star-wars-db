2b4bb1a198c266d0b4fcc86395bb30200a3c54ce
Evgeniy Grishko <evgeniygrish@gmail.com>
2022-07-04 10:10:23 +0300

    adds onGetAllPeople func

--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,6 +1,27 @@
 import { Component } from "react";
+import ApiService from "../../servises/ApiService";
 
 class ItemList extends Component {
+  apiService = new ApiService();
+
+  state = {
+    allPeople: {},
+  };
+
+  componentDidMount() {
+    this.onGetAllPeople();
+  }
+
+  onGetAllPeople = async () => {
+    try {
+      const allPeople = await this.apiService.getAllPeople();
+
+      return this.setState({ allPeople: allPeople });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   render() {
     return (
       <ul className="item-list list-group">
